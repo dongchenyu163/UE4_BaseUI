8f9c46fced667243e1775c6cf1805d9c4314c19a
Dong <dongchenyu164@gmail.com>
2022-06-23 13:34:36 +0900

    【K2Node】初始化

---
+++ Source/BaseUI_K2Nodes/BaseUI_K2Nodes.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class BaseUI_K2Nodes : ModuleRules
+{
+    public BaseUI_K2Nodes(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/BaseUI_K2Nodes/Private/BaseUI_K2Nodes.cpp
@@ -0,0 +1,16 @@
+﻿#include "BaseUI_K2Nodes.h"
+
+#define LOCTEXT_NAMESPACE "FBaseUI_K2NodesModule"
+
+void FBaseUI_K2NodesModule::StartupModule()
+{
+}
+
+void FBaseUI_K2NodesModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FBaseUI_K2NodesModule, BaseUI_K2Nodes)
