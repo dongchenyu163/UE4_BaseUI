653852736a40f1746924ff5889a5e84a4e489928
Dong <dongchenyu164@gmail.com>
2022-06-29 21:10:10 +0900

    【GI】注释掉：头文件中 需要移动到其他Handler的函数以及不需要的变量。

--- Source/BaseUI_Module/Public/GameplayClasses/GameInstanceBase/Interfaces/I_GI_MenuFramework.h
+++ Source/BaseUI_Module/Public/GameplayClasses/GameInstanceBase/Interfaces/I_GI_MenuFramework.h
@@ -36,48 +36,48 @@ public:
 	 * BaseHandler对象获取函数区域
 	 *******************************/
 #pragma region 功能函数区域
-	// UResumeMenuBaseHandler
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="返回主菜单时用"))
-	TSoftObjectPtr<UWorld> GetMainMenuMap();
-	
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
-	UDataTable* GetMapInfoDataTable();
-	// USaveBaseHandler
-	virtual UDataTable* GetMapInfoDataTable_CPP() = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
-	FMapUIInfo GetMapUIInfo(FName InMapIdentifier);
-	// UNextLevelBaseHandler  UAC_CTRL_MenuBase
-	virtual FMapUIInfo* GetMapUIInfo_CPP(FName InMapIdentifier) = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="USaveBaseHandler保存UserGlobalData的时候需要的地图进度信息"))
-	TMap<FName, FMapUIRuntimeInfo> GetUIRuntimeInfoTMap();
-	virtual TMap<FName, FMapUIRuntimeInfo>* GetUIRuntimeInfoTMap_CPP() = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
-	FMapUIRuntimeInfo GetMapUIRuntimeInfo(FName InMapIdentifier);
-	virtual FMapUIRuntimeInfo* GetMapUIRuntimeInfo_CPP(FName InMapIdentifier) = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="通过UserGlobalData来更新GameInstance里面的成员"))
-	void UpdateFromUserGlobalData();
-	virtual void UpdateFromUserGlobalData_CPP() = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
-	FMapUIInfo GetPlayingMapUIInfo();
-	// USaveBaseHandler UNextLevelBaseHandler
-	virtual FMapUIInfo* GetPlayingMapUIInfo_CPP() = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
-	void SetPlayingMapUIInfo(FMapUIInfo InNewMapInfo);
-	// UMapSelectionBaseHandler
-	virtual void SetPlayingMapUIInfo_CPP(const FMapUIInfo* InNewMapInfo) = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="重置UDA_WidgetInfo中的WidgetRef变量，切换地图的时候该变量会被置空"))
-	void ResetWidgetInfo();
-	// UMapSelectionBaseHandler
-	virtual void ResetWidgetInfo_CPP() = 0;
-
-	virtual FOnAnyWorldBeginPlay* GetOnAnyWorldBeginPlayDelegate_CPP() = 0;
+	// // UResumeMenuBaseHandler
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="返回主菜单时用"))
+	// TSoftObjectPtr<UWorld> GetMainMenuMap();
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
+	// UDataTable* GetMapInfoDataTable();
+	// // USaveBaseHandler
+	// virtual UDataTable* GetMapInfoDataTable_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
+	// FMapUIInfo GetMapUIInfo(FName InMapIdentifier);
+	// // UNextLevelBaseHandler  UAC_CTRL_MenuBase
+	// virtual FMapUIInfo* GetMapUIInfo_CPP(FName InMapIdentifier) = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="USaveBaseHandler保存UserGlobalData的时候需要的地图进度信息"))
+	// TMap<FName, FMapUIRuntimeInfo> GetUIRuntimeInfoTMap();
+	// virtual TMap<FName, FMapUIRuntimeInfo>* GetUIRuntimeInfoTMap_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
+	// FMapUIRuntimeInfo GetMapUIRuntimeInfo(FName InMapIdentifier);
+	// virtual FMapUIRuntimeInfo* GetMapUIRuntimeInfo_CPP(FName InMapIdentifier) = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="通过UserGlobalData来更新GameInstance里面的成员"))
+	// void UpdateFromUserGlobalData();
+	// virtual void UpdateFromUserGlobalData_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
+	// FMapUIInfo GetPlayingMapUIInfo();
+	// // USaveBaseHandler UNextLevelBaseHandler
+	// virtual FMapUIInfo* GetPlayingMapUIInfo_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction")
+	// void SetPlayingMapUIInfo(FMapUIInfo InNewMapInfo);
+	// // UMapSelectionBaseHandler
+	// virtual void SetPlayingMapUIInfo_CPP(const FMapUIInfo* InNewMapInfo) = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|BaseFunction", meta=(ToolTip="重置UDA_WidgetInfo中的WidgetRef变量，切换地图的时候该变量会被置空"))
+	// void ResetWidgetInfo();
+	// // UMapSelectionBaseHandler
+	// virtual void ResetWidgetInfo_CPP() = 0;
+	//
+	// virtual FOnAnyWorldBeginPlay* GetOnAnyWorldBeginPlayDelegate_CPP() = 0;
 #pragma endregion 功能函数区域
 
 	
@@ -85,30 +85,30 @@ public:
 	 * BaseHandler对象获取函数区域
 	 *******************************/
 #pragma region BaseHandler对象获取函数区域
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
-	TScriptInterface<II_UI_SinglePlayerFunctions> GetSinglePlayerMenuBaseHandler();
-	virtual II_UI_SinglePlayerFunctions* GetSinglePlayerMenuBaseHandler_CPP() = 0;
-	
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
-	TScriptInterface<II_UI_MapSelectable> GetMapSelector();
-	virtual II_UI_MapSelectable* GetMapSelector_CPP() = 0;
-	
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
-	TScriptInterface<II_UI_ResumeMenu> GetResumeMenuBaseHandler();
-	virtual II_UI_ResumeMenu* GetResumeMenuBaseHandler_CPP() = 0;
-	
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
-	TScriptInterface<II_UI_Savable> GetSavableBaseHandler();
-	virtual II_UI_Savable* GetSavableBaseHandler_CPP() = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
-	TScriptInterface<II_UI_NextLevel> GetNextLevelHandler();
-	virtual II_UI_NextLevel* GetNextLevelHandler_CPP() = 0;
-
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
-	TScriptInterface<II_UserManager> GetUserManager();
-	virtual II_UserManager* GetUserManager_CPP() = 0;
-
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
+	// TScriptInterface<II_UI_SinglePlayerFunctions> GetSinglePlayerMenuBaseHandler();
+	// virtual II_UI_SinglePlayerFunctions* GetSinglePlayerMenuBaseHandler_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
+	// TScriptInterface<II_UI_MapSelectable> GetMapSelector();
+	// virtual II_UI_MapSelectable* GetMapSelector_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
+	// TScriptInterface<II_UI_ResumeMenu> GetResumeMenuBaseHandler();
+	// virtual II_UI_ResumeMenu* GetResumeMenuBaseHandler_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
+	// TScriptInterface<II_UI_Savable> GetSavableBaseHandler();
+	// virtual II_UI_Savable* GetSavableBaseHandler_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
+	// TScriptInterface<II_UI_NextLevel> GetNextLevelHandler();
+	// virtual II_UI_NextLevel* GetNextLevelHandler_CPP() = 0;
+	//
+	// UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="GameInstance|MenuFramework|Handler Getter")
+	// TScriptInterface<II_UserManager> GetUserManager();
+	// virtual II_UserManager* GetUserManager_CPP() = 0;
+	//
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="FunctionHandler", meta=(DisplayName="", Tooltip=""))
 	UFunctionHandlerBase* FindHandler_ByName(FName InHandlerName);
 	virtual UFunctionHandlerBase* FindHandler_ByName_CPP(FName InHandlerName) = 0;
--- Source/BaseUI_Module/Public/GameplayClasses/GameInstanceBase/MainMenuGameInstanceBase.h
+++ Source/BaseUI_Module/Public/GameplayClasses/GameInstanceBase/MainMenuGameInstanceBase.h
@@ -28,107 +28,62 @@ protected:
 public:
 	virtual UFunctionHandlerBase* FindHandler_ByName_CPP(FName InHandlerName) override;
 	virtual UFunctionHandlerBase* FindHandler_ByName_Implementation(FName InHandlerName) override { return FindHandler_ByName_CPP(InHandlerName); }
-
-	virtual TScriptInterface<II_UI_SinglePlayerFunctions> GetSinglePlayerMenuBaseHandler_Implementation() override { return SinglePlayerFunctionsHandlerObj; }
-	virtual TScriptInterface<II_UI_MapSelectable> GetMapSelector_Implementation() override { return MapSelectableHandlerObj; }
-	virtual TScriptInterface<II_UI_ResumeMenu> GetResumeMenuBaseHandler_Implementation() override { return ResumeMenuHandlerObj; }
-	virtual TScriptInterface<II_UI_Savable> GetSavableBaseHandler_Implementation() override { return SavableHandlerObj; }
-	virtual TScriptInterface<II_UI_NextLevel> GetNextLevelHandler_Implementation() override { return NextLevelHandlerObj; }
-	virtual TScriptInterface<II_UserManager> GetUserManager_Implementation() override { return UserManager; }
 	
-	virtual II_UI_SinglePlayerFunctions* GetSinglePlayerMenuBaseHandler_CPP() override { return SinglePlayerFunctionsHandlerObj; }
-	virtual II_UI_MapSelectable* GetMapSelector_CPP() override { return MapSelectableHandlerObj; }
-	virtual II_UI_ResumeMenu* GetResumeMenuBaseHandler_CPP() override { return ResumeMenuHandlerObj; }
-	virtual II_UI_Savable* GetSavableBaseHandler_CPP() override { return SavableHandlerObj; }
-	virtual II_UI_NextLevel* GetNextLevelHandler_CPP() override { return NextLevelHandlerObj; }
-	virtual II_UserManager* GetUserManager_CPP() override { return UserManager; }
-	
-	virtual TSoftObjectPtr<UWorld> GetMainMenuMap_Implementation() override { return MainMenuMap; }
+	// virtual TSoftObjectPtr<UWorld> GetMainMenuMap_Implementation() override { return MainMenuMap; }
+	//
+	// virtual UDataTable* GetMapInfoDataTable_CPP() override { return MapInfoDataTable; }
+	// virtual UDataTable* GetMapInfoDataTable_Implementation() override;
+	//
+	// virtual TMap<FName, FMapUIRuntimeInfo>* GetUIRuntimeInfoTMap_CPP() override { return &Map_MapName_To_MapRuntimeInfo; }
+	// virtual TMap<FName, FMapUIRuntimeInfo> GetUIRuntimeInfoTMap_Implementation() override { return *GetUIRuntimeInfoTMap_CPP(); }
+	// virtual FMapUIInfo GetMapUIInfo_Implementation(FName InMapIdentifier) override;
+	// virtual FMapUIRuntimeInfo GetMapUIRuntimeInfo_Implementation(FName InMapIdentifier) override;
+	// virtual FMapUIInfo* GetMapUIInfo_CPP(FName InMapIdentifier) override;
+	// virtual FMapUIRuntimeInfo* GetMapUIRuntimeInfo_CPP(FName InMapIdentifier) override;
+	//
+	// virtual void UpdateFromUserGlobalData_CPP() override;
+	// virtual void UpdateFromUserGlobalData_Implementation() override { UpdateFromUserGlobalData_CPP(); }
 
-	virtual UDataTable* GetMapInfoDataTable_CPP() override { return MapInfoDataTable; }
-	virtual UDataTable* GetMapInfoDataTable_Implementation() override;
-
-	virtual TMap<FName, FMapUIRuntimeInfo>* GetUIRuntimeInfoTMap_CPP() override { return &Map_MapName_To_MapRuntimeInfo; }
-	virtual TMap<FName, FMapUIRuntimeInfo> GetUIRuntimeInfoTMap_Implementation() override { return *GetUIRuntimeInfoTMap_CPP(); }
-	virtual FMapUIInfo GetMapUIInfo_Implementation(FName InMapIdentifier) override;
-	virtual FMapUIRuntimeInfo GetMapUIRuntimeInfo_Implementation(FName InMapIdentifier) override;
-	virtual FMapUIInfo* GetMapUIInfo_CPP(FName InMapIdentifier) override;
-	virtual FMapUIRuntimeInfo* GetMapUIRuntimeInfo_CPP(FName InMapIdentifier) override;
-
-	virtual void UpdateFromUserGlobalData_CPP() override;
-	virtual void UpdateFromUserGlobalData_Implementation() override { UpdateFromUserGlobalData_CPP(); }
-
-	virtual FMapUIInfo* GetPlayingMapUIInfo_CPP() override;
-	virtual void SetPlayingMapUIInfo_CPP(const FMapUIInfo* InNewMapInfo) override;
-	virtual FMapUIInfo GetPlayingMapUIInfo_Implementation() override;
-	virtual void SetPlayingMapUIInfo_Implementation(FMapUIInfo InNewMapInfo) override;
-	virtual void ResetWidgetInfo_CPP() override;
-	virtual void ResetWidgetInfo_Implementation() override;
+	// virtual FMapUIInfo* GetPlayingMapUIInfo_CPP() override;
+	// virtual void SetPlayingMapUIInfo_CPP(const FMapUIInfo* InNewMapInfo) override;
+	// virtual FMapUIInfo GetPlayingMapUIInfo_Implementation() override;
+	// virtual void SetPlayingMapUIInfo_Implementation(FMapUIInfo InNewMapInfo) override;
+	// virtual void ResetWidgetInfo_CPP() override;
+	// virtual void ResetWidgetInfo_Implementation() override;
 protected:
 	virtual void OnStart() override;
 	virtual void OnFirstWorldChanged(UWorld* NewWorld);
 public:
 	virtual void OnWorldChanged(UWorld* OldWorld, UWorld* NewWorld) override;
-	virtual FOnAnyWorldBeginPlay* GetOnAnyWorldBeginPlayDelegate_CPP() override { return &OnAnyWorldBeginPlay; }
+	// virtual FOnAnyWorldBeginPlay* GetOnAnyWorldBeginPlayDelegate_CPP() override { return &OnAnyWorldBeginPlay; }
 
-	virtual FMapUIInfo* FindMapUIInfo_ByUWorld(UWorld* InWorldPtr);
+	// virtual FMapUIInfo* FindMapUIInfo_ByUWorld(UWorld* InWorldPtr);
 	static void InitHandlers(II_GI_MenuFramework* InGameInstance, TMap<FName, FFunctionHandlerInfo>& InHandlerClassDict, TMap<FName, UFunctionHandlerBase*>& InCreatedHandlerDict);
 	static void CreateHandlers(TMap<FName, FFunctionHandlerInfo>& InHandlerClassDict, TMap<FName, UFunctionHandlerBase*>& OutCreatedHandlerDict, UObject* InObjectOuter = GetTransientPackage());
 protected:
 
-	UPROPERTY()
-	TMap<FName, UFunctionHandlerBase*> Map_HandlerName_To_HandlerObj;
+	
 	UPROPERTY()  // 只是为了保持该变量不被回收
-	USinglePlayerMenuBaseHandler* SinglePlayerFunctionsHandlerObj = nullptr;
-	UPROPERTY()
-	UMapSelectionBaseHandler* MapSelectableHandlerObj = nullptr;
-	UPROPERTY()
-	UResumeMenuBaseHandler* ResumeMenuHandlerObj = nullptr;
-	UPROPERTY()
-	USaveBaseHandler* SavableHandlerObj = nullptr;
-	UPROPERTY()
-	UNextLevelBaseHandler* NextLevelHandlerObj = nullptr;
-
-	UPROPERTY()
-	UUserManagerBase* UserManager = nullptr;
-	UPROPERTY()
-	USavingBaseHandler* SavingBaseHandler = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<USinglePlayerMenuBaseHandler> SinglePlayerMenuHandlerClass = USinglePlayerMenuBaseHandler::StaticClass();
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<UMapSelectionBaseHandler> MapSelectableHandlerClass = UMapSelectionBaseHandler::StaticClass();
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<UResumeMenuBaseHandler> ResumeMenuHandlerClass = UResumeMenuBaseHandler::StaticClass();
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<USaveBaseHandler> SavableHandlerClass = USaveBaseHandler::StaticClass();
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<UNextLevelBaseHandler> NextLevelHandlerClass = UNextLevelBaseHandler::StaticClass();
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<UUserManagerBase> UserManagerClass = UUserManagerBase::StaticClass();
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Handler Class", meta=(Tooltip=""))
-	TSubclassOf<USavingBaseHandler> SavingBaseHandlerClass = USavingBaseHandler::StaticClass();
-
+	TMap<FName, UFunctionHandlerBase*> Map_HandlerName_To_HandlerObj;
 
 	/**
 	 *
 	 */
 public:
-	FOnAnyWorldBeginPlay OnAnyWorldBeginPlay; 
+	// FOnAnyWorldBeginPlay OnAnyWorldBeginPlay; 
 
-protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo")
-	TSoftObjectPtr<UWorld> MainMenuMap = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo")
-	UDataTable* MapInfoDataTable = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo", meta=(Tooltip=""))
-	TMap<FName, FMapUIRuntimeInfo> Map_MapName_To_MapRuntimeInfo;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo", meta=(Tooltip=""))
-	FMapUIInfo PlayingMapInfo;
+// protected:
+// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo")
+// 	TSoftObjectPtr<UWorld> MainMenuMap = nullptr;
+//
+// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo")
+// 	UDataTable* MapInfoDataTable = nullptr;
+//
+// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo", meta=(Tooltip=""))
+// 	TMap<FName, FMapUIRuntimeInfo> Map_MapName_To_MapRuntimeInfo;
+//
+// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameInfo", meta=(Tooltip=""))
+// 	FMapUIInfo PlayingMapInfo;
 protected:
 	bool bIsInit = true;
 
